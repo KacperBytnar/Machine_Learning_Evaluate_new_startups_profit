f5a70120aaeba08644985291117d19423564381c
Mateista <kacper.bytnar19@gmail.com>
2023-03-14 23:29:21 +0100

    Moving functions to a library

--- calcs/parker.py
+++ calcs/parker.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import matplotlib as plt
 import numpy as np
+<<<<<<< HEAD
 from zlib import crc32
+=======
+>>>>>>> ff41db6 (Moving functions to a library)
 
 
 # Function that makes a dataset of randoms
@@ -12,12 +15,17 @@ def shuffle_and_split_data(data, test_ratio):
     train_indices = shuffled_indices[test_set_size:]
     return data.iloc[train_indices], data.iloc[test_indices]
  
+<<<<<<< HEAD
 
 # Function that makes a dataset of randoms
+=======
+ 
+>>>>>>> ff41db6 (Moving functions to a library)
 def is_id_in_test_set(identifier, test_ratio):
     return crc32(np.int64(identifier)) < test_ratio * 2**32
 
 
+<<<<<<< HEAD
 # Function that makes a dataset of randoms
 def split_data_with_id_hash(data, test_ratio, id_column):
     ids = data[id_column]
@@ -29,3 +37,9 @@ def split_data_with_id_hash(data, test_ratio, id_column):
 def scatter_matrix(data):
     pd.plotting.scatter_matrix(data, alpha=0.2, figsize=(12, 12), diagonal='kde')
     plt.show()
+=======
+def split_data_with_id_hash(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: is_id_in_test_set(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+>>>>>>> ff41db6 (Moving functions to a library)
